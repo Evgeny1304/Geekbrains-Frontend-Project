c74cf8cfb80d37b1ca726fae3709889284d34701
Evgeny1304 <erhabirov@gmail.com>
2019-04-22 23:07:46 +0300

    Курсовой проект. Реализован функционал корзины. Реализовано написание отзывов, их одобрение/удаление. Реализована авторизация в части JS (выход из аккаунта; смена пароля). Требуется доработка HTML с последующей корректировкой скриптов.

--- public/js/main.js
+++ public/js/main.js
@@ -35,7 +35,7 @@ Vue.component('product-item', {
         </div>
         <div class="product-box__description">
             <p class="product-box__text">{{item.name}}</p>
-            <p class="product-box__price">{{item.price}}</p>
+            <p class="product-box__price">\${{item.price}}</p>
         </div>
     </div>        
   `,
@@ -121,7 +121,7 @@ Vue.component('cart', {
                          <i class="fa fa-star" aria-hidden="true"></i>
                      </a>
                   </div>
-                    <p class="box-cart__price">{{item.quantity}} x {{item.price}}</p>
+                    <p class="box-cart__price">{{item.quantity}} x \${{item.price}}</p>
                </div>
              <button type="button" class="box-cart__icon" @click="handleDeleteClick(item)">
                  <i class="fas fa-times-circle"></i>
@@ -130,16 +130,55 @@ Vue.component('cart', {
         </ul>
         <div class="box-cart__total d-flex space-btw">
             <p class="box-cart__text">TOTAL</p>
-            <p class="box-cart__text">{{total}}</p>
+            <p class="box-cart__text">\${{total}}</p>
         </div>
-        <a href="#" class="box-cart__btn button">
+        <a href="checkout.html" class="box-cart__btn button">
            Checkout
         </a>
-        <a href="#" class="box-cart__btn button">
+        <a href="shopping_cart.html" class="box-cart__btn button">
             Go to cart
         </a>   
     </div>
   `,
+});
+
+Vue.component('feedback-list', {
+    props: ['feedback'],
+    methods: {
+        handleApproveFeedback(item) {
+            this.$emit('approve', item);
+        },
+        handleDeleteFeedback(item) {
+            this.$emit('delete', item);
+        },
+    },
+    template: `
+        <div class="comments">
+        <ul class="d-flex">
+        <li v-for="item in feedback">
+        <h3>{{item.name}}</h3>
+        <p>{{item.comment}}</p>
+        <button @click.prevent="handleApproveFeedback(item)">Approve</button>
+        <button @click.prevent="handleDeleteFeedback(item)">Delete</button>
+</li>
+</ul>
+        </div>
+    `,
+});
+
+Vue.component('feedback-approved', {
+    props: ['approved'],
+    template: `
+        <div class="comments">
+        <ul class="d-flex">
+        <li v-for="item in approved">
+        <h3>{{item.name}}</h3>
+        <p>{{item.mail}}</p>
+        <p>{{item.comment}}</p>
+</li>
+</ul>
+        </div>
+    `,
 });
 
 const app = new Vue({
@@ -147,10 +186,18 @@ const app = new Vue({
     data: {
         items: [],
         cart: [],
-        bigCart: [],
         filterValue: '',
         menuItems: [],
         searchQuery: '',
+        feedback: [],
+        approved: [],
+        users: [],
+        name: '',
+        mail: '',
+        login: '',
+        password: '',
+        changePassword: '',
+        comment: '',
         isVisibleCart: false,
     },
     mounted() {
@@ -159,11 +206,40 @@ const app = new Vue({
             .then((items) => {
                 this.cart = items;
             });
+
+        fetch(`${API_URL}/feedback`)
+            .then((response) => response.json())
+            .then((items) => {
+                this.feedback= items;
+            });
+
+        fetch(`${API_URL}/feedback_approve`)
+            .then((response) => response.json())
+            .then((items) => {
+                this.approved= items;
+            });
+
+        fetch(`${API_URL}/users`)
+            .then((response) => response.json())
+            .then((items) => {
+                this.users= items;
+            });
     },
     computed: {
         totalQuantity() {
             return this.cart.reduce((acc, item) => acc + item.quantity, 0);
-        }
+        },
+        totalSum() {
+            return this.cart.reduce((acc, item) => acc + item.quantity * item.price, 0);
+        },
+        getUser(){
+           let loginReg = localStorage.getItem('user');
+           if (loginReg === null){
+               return 'My Account';
+           } else {
+               return loginReg;
+           }
+        },
     },
     methods: {
         handleSearchClick(query) {
@@ -222,6 +298,105 @@ const app = new Vue({
                         this.cart = this.cart.filter((cartItem) => cartItem.id !== item.id);
                     });
             }
+        },
+        handleDeleteCart(){
+          fetch(`${API_URL}/cart`,{
+              method: 'DELETE',
+          })
+              .then(() => {
+                  this.cart = [];
+              });
+        },
+        handleAddFeedback(item) {
+            fetch(`${API_URL}/feedback`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: this.name,
+                    mail: this.mail,
+                    comment: this.comment
+                })
+            })
+                .then((response) => response.json())
+                .then((item) => {
+                    this.feedback.push(item);
+                });
+        },
+        handleApproveFeedback(item) {
+            fetch(`${API_URL}/feedback_approve`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(item)
+            })
+                .then((response) => response.json())
+                .then((item) => {
+                    this.approved.push(item);
+                });
+            fetch(`${API_URL}/feedback/${item.id}`, {
+                method: 'DELETE',
+            })
+                .then(() => {
+                    this.feedback = this.feedback.filter((comment) => comment.id !== item.id);
+                });
+        },
+        handleDeleteFeedback(item){
+            fetch(`${API_URL}/feedback/${item.id}`, {
+                method: 'DELETE',
+            })
+                .then(() => {
+                    this.feedback = this.feedback.filter((comment) => comment.id !== item.id);
+                });
+        },
+        handleSignUp(item) {
+            fetch(`${API_URL}/users`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    login: this.login,
+                    password: this.password
+                })
+            })
+                .then((response) => response.json())
+                .then((item) => {
+                    this.users.push(item);
+                });
+        },
+        handleSignIn(){
+            const userReg = this.users.find((user) => user.login === this.login && user.password === this.password);
+            if (userReg === undefined){
+                localStorage.clear();
+                console.log('Error');
+            }else {
+                localStorage.setItem('user', userReg.login);
+                localStorage.setItem('userId', userReg.id);
+            }
+            window.location.reload();
+        },
+        handleLogOut(){
+            localStorage.removeItem('user');
+            localStorage.removeItem('userId');
+            window.location.reload();
+        },
+        handleChangePass(item){
+            item.id = localStorage.getItem('userId');
+                fetch(`${API_URL}/users/${item.id}`, {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({password: this.changePassword}),
+                })
+                    .then((response) => response.json())
+                    .then((item) => {
+                        const itemIdx = this.users.findIndex((entry) => entry.id === item.id);
+                        Vue.set(this.users, itemIdx, item);
+                    });
         }
     }
 });
